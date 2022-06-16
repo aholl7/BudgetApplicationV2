ec7efc2e2a923a43ed59333c52eea225731cddcf
aholl7 <67927550+aholl7@users.noreply.github.com>
2022-06-15 19:56:26 -0500

    Apostraphe solution

--- components/DeleteAlert.js
+++ components/DeleteAlert.js
@@ -36,7 +36,7 @@ const DeleteAlert = (props) => {
               </AlertDialogHeader>
   
               <AlertDialogBody>
-                Are you sure? You can't undo this action afterwards.
+                Are you sure? You can&apos;t undo this action afterwards.
               </AlertDialogBody>
   
               <AlertDialogFooter>
